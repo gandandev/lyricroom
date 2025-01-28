adc31ba158d8fd54230c1f6d907090014522de72
gandan <188059221+gandandev@users.noreply.github.com>
2025-01-28 21:27:00 +0900

    feat(player): add a basic player layout structure

--- src/routes/player/+page.svelte
+++ src/routes/player/+page.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
-  import { media, rawLrc } from '$lib/stores'
+  import { lrc, media, rawLrc } from '$lib/stores'
   import Warning from '~icons/material-symbols/warning-rounded'
   import Back from '~icons/material-symbols/arrow-back-ios-new-rounded'
 </script>
 
 {#if $media && $rawLrc}
-  <!-- svelte-ignore a11y_media_has_caption -->
-  <!-- TODO: Add video captions using the LRC file -->
-  <video src={URL.createObjectURL($media)} autoplay controls></video>
-  <pre>{$rawLrc}</pre>
+  <div class="flex h-screen items-center justify-between gap-24 px-24">
+    <!-- svelte-ignore a11y_media_has_caption -->
+    <!-- TODO: Add video captions using the LRC file -->
+    <video src={URL.createObjectURL($media)} class="h-fit max-w-[40%] rounded-xl" autoplay></video>
+    <div class="h-screen overflow-y-auto">
+      <div class="my-[50%] flex flex-col gap-2">
+        {#each $lrc?.scripts!.map((script) => script.text) ?? [] as lyric}
+          <span class="text-3xl">{lyric}</span>
+        {/each}
+      </div>
+    </div>
+  </div>
 {:else}
   <div class="flex h-screen items-center justify-center text-neutral-600">
     <div class="relative flex flex-col items-center gap-2">
