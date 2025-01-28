14179a129df52a85b1157881dbc6b5b2e563532a
gandan <188059221+gandandev@users.noreply.github.com>
2025-01-28 16:16:37 +0900

    feat(player): add basic player page

--- src/routes/player/+page.svelte
+++ src/routes/player/+page.svelte
@@ -1,1 +1,27 @@
-player
+<script lang="ts">
+  import { media, rawLrc } from '$lib/stores'
+  import Warning from '~icons/material-symbols/warning-rounded'
+  import Back from '~icons/material-symbols/arrow-back-ios-new-rounded'
+</script>
+
+{#if $media && $rawLrc}
+  <!-- svelte-ignore a11y_media_has_caption -->
+  <!-- TODO: Add video captions using the LRC file -->
+  <video src={URL.createObjectURL($media)} autoplay controls></video>
+  <pre>{$rawLrc}</pre>
+{:else}
+  <div class="flex h-screen items-center justify-center text-neutral-600">
+    <div class="relative flex flex-col items-center gap-2">
+      <div class="absolute inset-0 -z-10 rounded-2xl bg-white/10 blur-3xl"></div>
+      <Warning class="size-24" />
+      <span class="text-center text-lg font-medium">No media or LRC file selected</span>
+      <a
+        href="/"
+        class="flex items-center gap-1 rounded-full bg-neutral-800 py-2 pl-3 pr-4 text-neutral-400 duration-150 hover:bg-neutral-700 active:scale-95"
+      >
+        <Back class="size-4" />
+        Go back
+      </a>
+    </div>
+  </div>
+{/if}
